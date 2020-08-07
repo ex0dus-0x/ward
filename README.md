9b2c8b9bea8383986d900d873a0df835a25dd372
ex0dus-0x <ex0dus@codemuch.tech>
2020-08-07 00:10:53 -0400

    Changes

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Introduction
 
-__ward__ is a binary hardening tool that helps protect against code injection attacks for Linux executables.
+__ward__ is a binary hardening tool that helps protect against code injection attacks for Linux executables. It provides an intuitive UI that enables
+users to readily sign and harden Linux apps they've built or installed in a portable manner in userspace. This is to ultimately help mitigate any attempts
+to hijack process runtimes by leveraging `LD_PRELOAD`, and any tampering attempts to the actual executable itself.
+
+Read more about it in my blog post [here]().
--- protect/detect.c
+++ protect/detect.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-
-// This will resolve at linking time
-extern char **environ;
-
-int main()
+int main(int argc, char *argv[], char **environ)
 {
-    long i, j;
+    int i, j;
     char env[] = "LD_PRELOAD";
     // Go through all environment strings, the end of the array
     // is marked by a null pointer.
@@ -16,13 +11,12 @@ int main()
         for(j = 0; env[j] != '\0' && environ[i][j] != '\0'; j++)
             if(env[j] != environ[i][j])
                 break;
+
         // If the complete chain was found
         if(env[j] == '\0')
         {
-            printf("LD_PRELOAD detected through environ\n");
             return 1;
         }
     }
-    printf("Environment is clean\n");
     return 0;
 }
