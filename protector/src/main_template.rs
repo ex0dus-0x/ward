1d7814e8113a6cfd65bae2e0ace9e6e7f32085d7
ex0dus-0x <ex0dus@codemuch.tech>
2020-08-23 20:34:07 -0400

    Enhance protector binary

--- protector/src/main_template.rs
+++ protector/src/main_template.rs
@@ -1,7 +1,24 @@
 use goblin::Object;
+use goblin::elf::Elf;
 
+use std::fs;
 use std::env;
+use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    // set program to parse as this current one
+    let prog: &Path = Path::new(&args[0]);
+    let buffer = fs::read(prog)?;
+
+    // parse out Elf binary format
+    let elf = match Elf::parse(&buffer) {
+        Ok(bin) => bin,
+        Err(e) => {
+            panic!(e);
+        }
+    };
 }
+
+
