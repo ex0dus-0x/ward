7ed3b96768a69f7cbe1fea58e67201bde3dd7f84
ex0dus-0x <ex0dus@codemuch.tech>
2020-12-09 16:34:03 -0500

    Update golang implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ward
 
 go 1.15
+
+require github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -1,4 +1,59 @@
 package main
 
+import (
+    "os"
+    "fmt"
+    "log"
+    "errors"
+
+    "github.com/urfave/cli/v2"
+)
+
+const (
+    Compiler string = "clang"
+    Description string = "Security-hardened binary notary"
+)
+
+
+
 func main() {
+    app := &cli.App {
+        Name: "ward",
+        Usage: Description,
+        Commands: []*cli.Command{
+            {
+                Name: "protect",
+                Usage: "Given a target binary, inject a self-protection runtime.",
+                Action: func(c *cli.Context) error {
+                    binary := c.Args().First()
+                    if binary == "" {
+                        return errors.New("No binary specified for protection runtime injection.")
+                    }
+
+                    // check if valid path and valid ELF binary
+
+                    // if everything is set, provision new
+
+                    fmt.Println("[*] Injecting self-protection into ", binary)
+                    return nil
+                },
+            },
+            {
+                Name: "verify",
+                Usage: "Ensures a protected binary has proper integrity.",
+                Action: func(c *cli.Context) error {
+                    binary := c.Args().First()
+                    if binary == "" {
+                        return errors.New("No binary specified.")
+                    }
+                    return nil
+                },
+            },
+        },
+    }
+
+    err := app.Run(os.Args)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
