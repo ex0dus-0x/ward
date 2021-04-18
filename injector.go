a908bbbd9ac92a13d2d659e08eaa87d3664bae81
ex0dus-0x <ex0dus@codemuch.tech>
2021-04-18 11:46:29 -0400

    Lint

--- injector.go
+++ injector.go
@@ -1,122 +1,121 @@
 package main
 
 import (
-    "os"
-    "log"
-    "path"
-    "bytes"
-    "errors"
-    "runtime"
-    "os/exec"
-    "io/ioutil"
-    "path/filepath"
-    "compress/zlib"
+	"bytes"
+	"compress/zlib"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
 
-    // extends support for mutating and writing ELFs
-    "github.com/Binject/debug/elf"
+	// extends support for mutating and writing ELFs
+	"github.com/Binject/debug/elf"
 )
 
 const (
-    Compiler string = "clang"
-    //StubSectionName string = ".ward.protect"
+	Compiler string = "clang"
+	//StubSectionName string = ".ward.protect"
 )
 
 // Helper that compiles a new stub application with `clang` to be packed.
 func Provision(name string, overwrite bool) (*string, error) {
 
-    // find directory to stub path in Golang package
-    _, filename, _, ok := runtime.Caller(0)
-    if !ok {
-        return nil, errors.New("Cannot find package path to provision stub program.")
-    }
+	// find directory to stub path in Golang package
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return nil, errors.New("Cannot find package path to provision stub program.")
+	}
 
-    // get current path before changing to stub path
-    cwd, err := os.Getwd()
-    if err != nil {
-        return nil, err
-    }
+	// get current path before changing to stub path
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
 
-    // get path to stub workspace
-    stubPath := filepath.Join(path.Dir(filename), "stub")
-    if err := os.Chdir(stubPath); err != nil {
-        return nil, err
-    }
+	// get path to stub workspace
+	stubPath := filepath.Join(path.Dir(filename), "stub")
+	if err := os.Chdir(stubPath); err != nil {
+		return nil, err
+	}
 
-    // if overwrite is set, rewrite the original path (might be dangerous)
-    var out string
-    if !overwrite {
-        out = filepath.Join(cwd, name + ".packed")
-    } else {
-        out, err = filepath.Abs(name)
-        if err != nil {
-            return nil, err
-        }
-    }
+	// if overwrite is set, rewrite the original path (might be dangerous)
+	var out string
+	if !overwrite {
+		out = filepath.Join(cwd, name+".packed")
+	} else {
+		out, err = filepath.Abs(name)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-    // create compilation command
-    cmd := exec.Command(Compiler, "-static", "-O2", "-D_FORTIFY_SOURCE=2", "-o",
-        out, "main.c", "runtime.c", "-lelf", "-lz")
+	// create compilation command
+	cmd := exec.Command(Compiler, "-static", "-O2", "-D_FORTIFY_SOURCE=2", "-o",
+		out, "main.c", "runtime.c", "-lelf", "-lz")
 
-    // execute compilation routine to generate a new binary
-    if err := cmd.Run(); err != nil {
-        return nil, err
-    }
+	// execute compilation routine to generate a new binary
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
 
-    // go back to original work directory
-    if err := os.Chdir(cwd); err != nil {
-        return nil, err
-    }
+	// go back to original work directory
+	if err := os.Chdir(cwd); err != nil {
+		return nil, err
+	}
 
-    // return path to newly compiled stub executable
-    return &out, nil
+	// return path to newly compiled stub executable
+	return &out, nil
 }
 
 // Defines an Injector object that consumes a path to a compiled stub and
 // target binary and creates a protected binary.
 type Injector struct {
-    Filepath string         // path to the stub host
-    Offset int64            // represents offset and filesize of stub
-    StubProgram *elf.File   // parsed ELF of the stub host
-    Target []byte           // parsed bytes of the target binary to protect
+	Filepath    string    // path to the stub host
+	Offset      int64     // represents offset and filesize of stub
+	StubProgram *elf.File // parsed ELF of the stub host
+	Target      []byte    // parsed bytes of the target binary to protect
 }
 
 // Create a new Injector interface to provision a runtime application
 func NewInjector(binpath string, stub string) (*Injector, error) {
 
-    // read bytes from target binary path we want to protect
-    targetBytes, err := ioutil.ReadFile(binpath)
-    if err != nil {
-        return nil, err
-    }
+	// read bytes from target binary path we want to protect
+	targetBytes, err := ioutil.ReadFile(binpath)
+	if err != nil {
+		return nil, err
+	}
 
-    // open to parse filesize
-    f, err := os.Stat(stub)
-    if err != nil {
-        return nil, err
-    }
-    fsize := f.Size()
+	// open to parse filesize
+	f, err := os.Stat(stub)
+	if err != nil {
+		return nil, err
+	}
+	fsize := f.Size()
 
-    // apply zlib compression
-    var buf bytes.Buffer
-    writer := zlib.NewWriter(&buf)
-    writer.Write(targetBytes)
-    writer.Close()
-    _ = buf.Bytes()
+	// apply zlib compression
+	var buf bytes.Buffer
+	writer := zlib.NewWriter(&buf)
+	writer.Write(targetBytes)
+	writer.Close()
+	_ = buf.Bytes()
 
-    // reopen and parse stub as ELF binary
-    binary, err := elf.Open(stub)
-    if err != nil {
-        return nil, err
-    }
+	// reopen and parse stub as ELF binary
+	binary, err := elf.Open(stub)
+	if err != nil {
+		return nil, err
+	}
 
-    return &Injector {
-        stub,
-        fsize,
-        binary,
-        targetBytes,
-    }, nil
+	return &Injector{
+		stub,
+		fsize,
+		binary,
+		targetBytes,
+	}, nil
 }
-
 
 /*
 // Helper used to modify the state of the shstrtab section in the ELF binary with the
@@ -156,40 +155,39 @@ func (inj *Injector) OverwriteSection() error {
 }
 */
 
-
 // Method used to inject the original host into the new stub one by using a modified
 // version of the PT_NOTE infection method, where the
 func (inj *Injector) InjectBinary() error {
 
-    // we only modify p_filesz to be size of packed ELF, and p_off to be offset in current file
-    log.Println("Finding PT_NOTE segment for injecting metadata")
-    for _, seg := range inj.StubProgram.Progs {
-        if seg.Type == elf.PT_NOTE {
-            seg.Filesz = uint64(len(inj.Target))
-            seg.Off = uint64(inj.Offset)
-            log.Printf("Offset: %d Size: %d\n", seg.Off, seg.Filesz)
-            break
-        }
-    }
+	// we only modify p_filesz to be size of packed ELF, and p_off to be offset in current file
+	log.Println("Finding PT_NOTE segment for injecting metadata")
+	for _, seg := range inj.StubProgram.Progs {
+		if seg.Type == elf.PT_NOTE {
+			seg.Filesz = uint64(len(inj.Target))
+			seg.Off = uint64(inj.Offset)
+			log.Printf("Offset: %d Size: %d\n", seg.Off, seg.Filesz)
+			break
+		}
+	}
 
-    // append target binary to the end of the stub host
-    log.Println("Writing (not yet encoded) ELF to stub")
-    inj.StubProgram.InsertionEOF = inj.Target
+	// append target binary to the end of the stub host
+	log.Println("Writing (not yet encoded) ELF to stub")
+	inj.StubProgram.InsertionEOF = inj.Target
 
-    // get bytes from final stub state
-    elfBytes, err := inj.StubProgram.Bytes()
-    if err != nil {
-        return nil
-    }
-    inj.StubProgram.Close()
+	// get bytes from final stub state
+	elfBytes, err := inj.StubProgram.Bytes()
+	if err != nil {
+		return nil
+	}
+	inj.StubProgram.Close()
 
-    // overwrite original stub with changes in ELF format
-    f, err := os.OpenFile(inj.Filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-    if err != nil {
-        return err
-    }
+	// overwrite original stub with changes in ELF format
+	f, err := os.OpenFile(inj.Filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
 
-    f.Write(elfBytes)
-    f.Close()
-    return nil
+	f.Write(elfBytes)
+	f.Close()
+	return nil
 }
--- main.go
+++ main.go
@@ -1,72 +1,71 @@
 package main
 
 import (
-    "os"
-    "log"
-    "errors"
-    "debug/elf"
+	"debug/elf"
+	"errors"
+	"log"
+	"os"
 
-
-    "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-    app := &cli.App {
-        Name: "ward",
-        Usage: "Dumb ELF packer",
-        Commands: []*cli.Command{
-            {
-                Name: "pack",
-                Usage: "Pack a target binary, and inject a self-protection runtime.",
-                Flags: []cli.Flag{
-                    &cli.BoolFlag{
-                        Name: "overwrite",
-                        Usage: "If set, overwrite the original target binary (NOT RECOMMENDED).",
-                        Aliases: []string{"o"},
-                    },
-                },
-                Action: func(c *cli.Context) error {
-                    log.Println("Starting up ward")
+	app := &cli.App{
+		Name:  "ward",
+		Usage: "Dumb ELF packer",
+		Commands: []*cli.Command{
+			{
+				Name:  "pack",
+				Usage: "Pack a target binary, and inject a self-protection runtime.",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "overwrite",
+						Usage:   "If set, overwrite the original target binary (NOT RECOMMENDED).",
+						Aliases: []string{"o"},
+					},
+				},
+				Action: func(c *cli.Context) error {
+					log.Println("Starting up ward")
 
-                    binary := c.Args().First()
-                    if binary == "" {
-                        return errors.New("No binary specified for packing.")
-                    }
+					binary := c.Args().First()
+					if binary == "" {
+						return errors.New("No binary specified for packing.")
+					}
 
-                    _, err := os.Stat(binary)
-                    if os.IsNotExist(err) {
-                        return errors.New("ELF file not found at path.")
-                    }
+					_, err := os.Stat(binary)
+					if os.IsNotExist(err) {
+						return errors.New("ELF file not found at path.")
+					}
 
-                    log.Println("Checking if valid ELF binary")
-                    if _, err := elf.Open(binary); err != nil {
-                        return errors.New("Cannot open and parse target as ELF binary.")
-                    }
+					log.Println("Checking if valid ELF binary")
+					if _, err := elf.Open(binary); err != nil {
+						return errors.New("Cannot open and parse target as ELF binary.")
+					}
 
-                    overwrite := c.Bool("overwrite")
+					overwrite := c.Bool("overwrite")
 
-                    log.Println("Provisioning stub program for packing")
-                    protector, err := Provision(binary, overwrite)
-                    if err != nil {
-                        return err
-                    }
+					log.Println("Provisioning stub program for packing")
+					protector, err := Provision(binary, overwrite)
+					if err != nil {
+						return err
+					}
 
-                    log.Println("Packing original executable into stub", binary)
-                    injector, err := NewInjector(binary, *protector)
-                    if err != nil {
-                        return err
-                    }
+					log.Println("Packing original executable into stub", binary)
+					injector, err := NewInjector(binary, *protector)
+					if err != nil {
+						return err
+					}
 
-                    injector.InjectBinary()
-                    log.Println("Done! Find the packed application at", *protector)
-                    return nil
-                },
-            },
-        },
-    }
+					injector.InjectBinary()
+					log.Println("Done! Find the packed application at", *protector)
+					return nil
+				},
+			},
+		},
+	}
 
-    err := app.Run(os.Args)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
